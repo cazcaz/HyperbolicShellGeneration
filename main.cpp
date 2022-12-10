aa43fd9f7771267cc49de036e1c95fe39c5290e9
Callum Elliott <callum.elliott.11@hotmail.com>
2022-12-10 14:59:33 +0000

    Fix error

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 int main(int, char**) {
     
     ShellParams parameters;
-    parameters.resolution = 100;
-    parameters.extensionLength = 0.01;
-    parameters.stiffLengthRatio = 1;
-    parameters.desiredCurvature = 0.001;
+    parameters.resolution = 500;
+    parameters.extensionLength = 0.1;
+    parameters.stiffLengthRatio = 0.00001;
+    parameters.desiredCurvature = 0.00001;
     ShellGen shellGenerator(parameters);
 
     for (int j = 1; j <= 10; j++){
+        parameters.stiffLengthRatio = 0.00001;
         parameters.desiredCurvature *= 10;
         for (int i = 1; i <= 10; i++){
             parameters.stiffLengthRatio *= 10;
