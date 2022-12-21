a1f0b79fe0e30e1e8705da69a6d0ad99dd6a1d51
Callum Elliott <callum.elliott.11@hotmail.com>
2022-12-21 11:12:07 +0000

    FINAL VERSION IN THIS BRANCH

--- main.cpp
+++ main.cpp
@@ -1,63 +1,13 @@
 #include "batchGen.h"
 
 int main(int, char**) {
-    double stiffLengthStart = 0.01;
-    double DCStart = 0.0001;
-    double stiffLengthEnd = 0.1;
-    double DCEnd = 0.01;
-    double ExLStart = 0.1;
-    double ExLEnd = 10;
-    int parameterSpaceResolution = 1;
+    
+    //To use, populate parameterList with parameters of surfaces to be calculated, then call batchGen to calcuate them
 
     std::vector<ShellParams> parameterList;
     BatchGen massCalcer;
 
-    std::vector<double> stiffLengthsCircum;
-    std::vector<double> stiffLengthsRadial;
-    std::vector<double> DCs;
-    std::vector<double> ExLs;
-    for (int i=0; i<=parameterSpaceResolution; i++){
-        stiffLengthsCircum.push_back((stiffLengthEnd-stiffLengthStart)/parameterSpaceResolution * i + stiffLengthStart);
-        stiffLengthsRadial.push_back((stiffLengthEnd-stiffLengthStart)/parameterSpaceResolution * i + stiffLengthStart);
-        DCs.push_back((DCEnd-DCStart)/parameterSpaceResolution * i + DCStart);
-        ExLs.push_back((ExLEnd-ExLStart)/parameterSpaceResolution * i + ExLStart);
-
-    }
-    
-    // for (double stiff : stiffLengths){
-    //     for (double DC : DCs) {
-    //         ShellParams parameters;
-    //         parameters.resolution = 500;
-    //         parameters.extensionLength = 0.1;
-    //         parameters.expansions = 500;
-    //         parameters.desiredCurvature = DC;
-    //         parameters.stiffLengthRatio = stiff;
-    //         parameterList.push_back(parameters);
-    //     }
-    // }
-
-        // for (double stiffLengthCirc : stiffLengthsCircum) {
-        //     for (double stiffLengthRad : stiffLengthsRadial) {
-        //     ShellParams parameters;
-        //     parameters.resolution = 10;
-        //     parameters.extensionLength = 0.1;
-        //     parameters.expansions = 9;
-        //     parameters.desiredCurvature = 0.001;
-        //     parameters.stiffLengthRatioCircum = stiffLengthCirc;
-        //     parameters.stiffLengthRatioRadial = stiffLengthRad;
-        //     parameterList.push_back(parameters);
-        //     }
-        // }
-
-        int orig = 1000;
-            ShellParams parameters;
-            parameters.resolution = 5000;
-            parameters.extensionLength = 10;
-            parameters.expansions = orig;
-            parameters.desiredCurvature = 0.001;
-            parameters.stiffLengthRatioCircum = 0.1;
-            parameters.stiffLengthRatioRadial = 1;
-            parameterList.push_back(parameters);
+    //Push parameters to the parameterList
 
     massCalcer.calculateAll(parameterList);
     return 0;
